3085e70ec5945272eeb5f133fe2bcab69a99d18f
Wibol <19147332+Wibol@users.noreply.github.com>
2022-08-27 16:24:25 +0200

    Update linkhelper.py

--- linkhelper.py
+++ linkhelper.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: UTF-8 -*-
 # This is a Hexchat addon based on 'link-title.py' (https://github.com/Poorchop/hexchat-scripts/blob/master/link-title.py) and 'shortn' (https://github.com/ishu3101/shortn/blob/master/shortn.py). Thanks to 'Pdog' and 'ishu3101' for their job.
-
+#
 # SETTINGS #
-exlist = ('#linux-es') # Channels exclusion list. Eg: ('#channel1') or ('#channel1','channel2').
-uagent = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0'} # A valid browser User-Agent
+canales = ('#linux-es',) # Channel exclusion list. Eg: ('#channel1',) or ('#channel1','channel2').
+apodos = ('NewsBot',) # Nick exclusion list. Eg: ('nickl1',) or ('nickl1','nick2').
+agente = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0'} # A valid browser User-Agent
+acortador = 'isgd' # You can choose between "isgd" and "vgd" shortener services.
 ############
 
 import hexchat
@@ -12,85 +13,90 @@ import html
 import requests
 import threading
 
-__module_name__ = 'LinkHelper'
+__module_name__ = 'LinkHelperBot'
 __module_author__ = 'Wibol'
-__module_version__ = '1.1'
-__module_description__ = 'Displays the "nick", "html title" and "short URL" in Hexchat when a HTTP URL is posted in IRC channels.'
+__module_version__ = '1.2'
+__module_description__ = 'A Python3 Hexchat addon that sends to IRC channels the "nick", "html title" and "short URL" when a HTTP URL is posted.'
 
-events = ('Channel Message', 'Channel Action',
-          'Channel Msg Hilight', 'Channel Action Hilight',
-          'Channel Notice', 'Server Notice',
-          'Private Message', 'Private Message to Dialog',
-          'Private Action', 'Private Action to Dialog')
+eventos = (
+    'Channel Message', 'Channel Action',
+    'Channel Msg Hilight', 'Channel Action Hilight',
+    'Channel Notice', 'Server Notice',
+    'Private Message', 'Private Message to Dialog',
+    'Private Action', 'Private Action to Dialog'
+)
 
-def print_title(url, chan, nick, mode, cont):
-    if 'http://' in url or 'https://' in url: # Checking HTTP URL
-        title = get_response(url, uagent) # Getting URL response avoiding User-Agent lockout
-        if (title != None) and (chan not in exlist): # Checking URL title exists and exclusion channel list
-            surl = shorten(url) # Getting short URL
-            msg = '\x0307{2}: \x03\x02{0} > \x02\x0310{4} \x03'.format(title, url, nick, mode, surl) # Formating message
-            cont.command('TIMER 1 DOAT {0} ECHO {1}'.format(chan, msg)) # Displaying URL info message
-    return
+apis = {
+    'isgd' : 'https://is.gd/create.php?format=json&url=',
+	'vgd' : 'https://v.gd/create.php?format=json&url='
+}
 
-def get_response(url, agent):
-    try:
-        response = requests.get(url, headers= agent, cookies= {'CONSENT': 'PENDING+944'})
-        response.close()
-        if response.ok:
-            if response.headers['content-type'].split('/')[0] == 'text':
-                html_doc = response.text
-                title = snarfer(html_doc)
-                title = html.unescape(title)
-                title = title.lstrip()
-                return title
+def event_cb(word, word_eol, userdata, attributes):
+    contexto = hexchat.get_context()
+    canal = contexto.get_info('channel')
+    apodo = hexchat.strip(word[0]) #Stripping nick attributes
+
+    if attributes.time: # ignore znc playback
+        return hexchat.EAT_HEXCHAT
+    
+    if (canal not in canales) and (apodo not in apodos):
+        for elemento in word[1].split():
+            palabra = hexchat.strip(elemento, -1, 3)
+            if palabra.endswith(','):
+                palabra = palabra[:-1]
+            if ('http://' in palabra) or ('https://' in palabra): # Checking is HTTP URL
+                threading.Thread(target=print_msg, args=(word[0], palabra, canal)).start()
             else:
-                hexchat.prnt('LinkHelper info: Not supported "Content-type" = ' + response.headers['content-type'] + '.')
-        else:
-            hexchat.prnt('LinkHelper info: HTTP code = ' + str(response.status_code))
-    except:
-        hexchat.prnt('LinkHelper info: No URL response.')
-        return None
-
-def snarfer(html_doc, encoding=''):
-    try:
-        snarf = html_doc[html_doc.index('<title')+6:html_doc.index('</title>')][:431]
-        snarf = snarf[snarf.index('>')+1:]
-    except ValueError:
-        snarf = ''
-    return snarf
-
-def shorten(url):
-    try: # Trying to shorten URL
-        api = 'https://tinyurl.com/api-create.php?url='
-        sresponse = requests.get(api + url)
-        surl = sresponse.text
-    except Exception as err:
-        hexchat.prnt('A shortening error occurred:', sresponse.status_code)
-        surl = url
-    return surl
-
-def event_cb(word, word_eol, userdata, attr):
-    # ignore znc playback
-    if attr.time:
-        return
-    
-    word = [(word[i] if len(word) > i else '') for i in range(4)]
-    cur_context = hexchat.get_context()
-    chan = cur_context.get_info('channel')
-    
-    for w in word[1].split():
-        stripped_word = hexchat.strip(w, -1, 3)
-        
-        url = stripped_word
-
-        if url.endswith(','):
-            url = url[:-1]
-                
-        threading.Thread(target=print_title, args=(url, chan, word[0], word[2], cur_context)).start()
+                continue
 
     return hexchat.EAT_NONE
 
-for event in events:
-    hexchat.hook_print_attrs(event, event_cb)
+def print_msg(apodo, larga, canal):
+    titulo = get_html(larga) # Getting URL response avoiding User-Agent lockout
+    if (titulo != None): # Checking URL title exists and exclusion channel list
+        corta = get_shortened(larga) # Getting short URL
+        mensaje = '\x0307{}: \x03\x02{} > \x02\x0310{} \x03'.format(apodo, titulo, corta) # Formating message
+        hexchat.command('TIMER 1 DOAT {} ECHO {}'.format(canal, mensaje)) # Displaying URL message
+
+def get_html(larga):
+    try:
+        respuesta = requests.get(larga, headers= agente, cookies= {'CONSENT': 'PENDING+944'}, timeout=(2,4))
+        respuesta.close()
+        if respuesta.ok:
+            if respuesta.headers['content-type'].split('/')[0] == 'text':
+                texto = respuesta.text
+                titulo = get_title(texto)
+                titulo = html.unescape(titulo)
+                titulo = titulo.strip()
+                return titulo
+            else:
+                hexchat.prnt('LinkHelperBot info: Not supported "Content-type" = ' + respuesta.headers['content-type'] + '.')
+        else:
+            hexchat.prnt('LinkHelperBot info: HTTP code = ' + str(respuesta.status_code))
+    except requests.exceptions.RequestException as error:
+        hexchat.prnt('LinkHelperBot info: No URL response. ' + error)
+        return None
+
+def get_title(texto):
+    try:
+        titulo = texto[texto.find('<title') + 6 : texto.find('</title>')][:431]
+        titulo = titulo[titulo.find('>') + 1 :]
+    except ValueError as error:
+        hexchat.prnt('LinkHelperBot info: A getting title error occurred. ' + str(error))
+        titulo = ''
+    return titulo
+
+def get_shortened(larga):
+    try: # Trying to shorten URL
+        respuesta = requests.get(apis[acortador] + larga, timeout=(2,4))
+        corta = respuesta.json()['shorturl']
+        respuesta.close()
+    except requests.exceptions.RequestException as error:
+        hexchat.prnt('LinkHelperBot info: A shortening error occurred. ' + error)
+        corta = larga
+    return corta
+
+for evento in eventos:
+    hexchat.hook_print_attrs(evento, event_cb)
 
 hexchat.prnt( 'Loaded ' +__module_name__ + ' ' + __module_version__ + ' by ' + __module_author__ + '. ' + __module_description__)
